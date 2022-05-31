1d92cc952b8fc4119db6e125be08ff030868bcac
Isabelly Lemos <isabellylemos.ml@gmail.com>
2022-05-31 16:35:27 -0300

    implementação de rotas

--- mangrove-front-end/src/app/app-routing.module.ts
+++ mangrove-front-end/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'entrar',component:EntrarComponent},
+  {path:'cadastrar',component:CadastrarComponent},
+  {path:'menu',component:MenuComponent},
+  {path:'rodape',component:RodapeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
