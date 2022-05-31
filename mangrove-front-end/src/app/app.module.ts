4bd5ed1c30223a1a75086caa3e7a0dce16e29f12
Isabelly Lemos <isabellylemos.ml@gmail.com>
2022-05-31 15:49:20 -0300

    criando componente cadastrar

--- mangrove-front-end/src/app/app.module.ts
+++ mangrove-front-end/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    EntrarComponent
+    EntrarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
