0c3e43afaa697c0ae6e84a6c605c0be678084463
Claudio Pereira <claudio.prepara0@gmail.com>
2022-06-09 18:08:50 -0300

    Criação dos services de produtos e categorias

--- mangrove-front-end/src/app/home/home.component.ts
+++ mangrove-front-end/src/app/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { CategoriaModel } from '../model/CategoriaModel';
+import { ProdutosModel } from '../model/ProdutosModel';
+import { UsuariosModel } from '../model/UsuariosModel';
+import { ProdutosService } from '../service/produtos.service';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +13,17 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class HomeComponent implements OnInit {
 
+  produto: ProdutosModel = new ProdutosModel()
+  listaProdutos: ProdutosModel[]
+  usuario: UsuariosModel = new UsuariosModel()
+  idUsuario = environment.id
+  categoria: CategoriaModel = new CategoriaModel()
+  listaCategorias: CategoriaModel[]
+  idCategoria: number
+  
   constructor(
-    private router: Router
+    private router: Router,
+    private produtoService: ProdutosService
   ) { }
 
   ngOnInit() {
