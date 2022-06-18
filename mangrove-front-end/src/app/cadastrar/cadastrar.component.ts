2d9fc8678f25e7549a95311f8068d44c8cc3e41f
barbarasalla <barbara.msalla@gmail.com>
2022-06-18 09:57:24 -0300

    adicionando metodo de autentificação para adm

--- mangrove-front-end/src/app/cadastrar/cadastrar.component.ts
+++ mangrove-front-end/src/app/cadastrar/cadastrar.component.ts
@@ -15,8 +15,11 @@ export class CadastrarComponent implements OnInit {
   usuarioModel: UsuariosModel = new UsuariosModel()
   confirmarSenha: string
   tipUsuario: string
-  
-  
+
+  hrefTipo: string
+
+  passAdm: string
+ 
   constructor(
     private auth : AuthService,
     private router : Router,
@@ -25,16 +28,29 @@ export class CadastrarComponent implements OnInit {
 
   ngOnInit() {
   window.scroll(0,0)
+  this.userAdm()
   }
   confirmaSenha(event: any){
     this.confirmarSenha = event.target.value
   }
+
   tipoDoUsuario(event: any){
     this.tipUsuario = event.target.value
+    this.userAdm()
   }
   
   cadastrar(){
-    this.usuarioModel.tipoUsuario = this.tipUsuario
+
+    if(this.tipUsuario == "adm"){
+      if(this.autenticarAdm()){
+        this.usuarioModel.tipoUsuario = this.tipUsuario
+      } else{
+        this.alertas.showAlertDanger('Administrador não autenticado.')
+      }
+    } else{
+      this.usuarioModel.tipoUsuario = this.tipUsuario
+    }
+
     if(this.usuarioModel.passwordUsuario != this.confirmarSenha){
       this.alertas.showAlertDanger('As senhas estão incorretas.')
     } else{
@@ -44,6 +60,26 @@ export class CadastrarComponent implements OnInit {
         this.alertas.showAlertSuccess('Usuário cadastrado com sucesso!')
       })     
     }
+  }
+
+  userAdm(){
+      let ok: boolean = false;
+      if (this.tipUsuario == 'adm') {
+        ok = true
+      }
+      return ok
+  }
+
+  senhaAdm(event: any){
+    this.passAdm = event.target.value
+  }
+
+  autenticarAdm(){
+    let ok: boolean = false;
+    if (this.passAdm == "adm357951") {
+      ok = true
+    }
+    return ok
   }
 
 }
